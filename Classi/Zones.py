7cb68435ed0d03e4014974fb2f448bdc8efaae3e
Federico Piras <fedepiras95@gmail.com>
2023-07-18 12:15:40 +0200

    upload_first_class_files

---
+++ Classi/Zones.py
@@ -0,0 +1,15 @@
+"""
+File creato da Federico Piras in data 18.07.2023
+
+Definizione di una classe zona
+
+"""
+
+
+class Zone:
+
+    def __init__(self, zone_name):
+        self.zone_name = zone_name
+
+    def get_zone_name(self):
+        return self.zone_name
---
+++ Classi/weapon_objects.py
@@ -0,0 +1,21 @@
+"""
+
+File creato da Federico Piras in data 18.07.2023
+
+Definizione delle armi.
+
+
+Sword: Spada ------------------> Cavaliere
+Bow: Arco.   ------------------> ELfo
+Magic wand: Bacchetta magica --> Mago.
+Shuriken: shuriken ------------> Ninja.
+
+"""
+
+from Weapons import Weapon
+
+sword = Weapon(name="sward", damage=11, price=10, level=0)
+bow = Weapon(name="bow", damage=5, price=5, level=0)
+magic_wand = Weapon(name="magic_wand", damage=12, price=7, level=0)
+shuriken = Weapon(name="shuriken", damage=9, price=8, level=0)
+
