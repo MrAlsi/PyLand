2f81a18cee99fbf019b16b5a75b316b82b6abca5
GianlucaRossi8 <gianly.rossi@gmail.com>
2023-07-19 10:59:42 +0200

    Aggiunta armi cavaliere

--- Classi/weapon_objects.py
+++ Classi/weapon_objects.py
@@ -16,6 +16,11 @@ from Weapons import Weapon
 
 # Definire armi per Knight
 sword = Weapon(name="sward", damage=11, price=10, level=0)
+lance = Weapon(name="lance", damage=15, price=15, level=1)
+mace = Weapon(name="mace", damage=13, price=13, level=1)
+axe = Weapon(name="axe", damage=12, price=12, level=1)
+bow = Weapon(name="bow", damage=10, price=10, level=1)
+dagger = Weapon(name="dagger", damage=8, price=8, level=1)
 
 # Definire armi per Elf
 bow = Weapon(name="bow", damage=5, price=5, level=0)
