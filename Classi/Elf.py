de6f0d32306de15073d6abb4fb45de862a9213e1
GianlucaRossi8 <gianly.rossi@gmail.com>
2023-07-19 17:36:48 +0200

    Agginta dell'attacco speciale per le classi personaggio

--- Classi/Elf.py
+++ Classi/Elf.py
@@ -6,4 +6,5 @@ from Classi.Character import Character
 
 class Elf(Character):
     def __init__(self, lineage, name, level, weapon, life, basic_attack, defence, special_attack, gender, exp, wallet, inventory):
-        super().__init__(lineage, name, level, weapon, life, basic_attack, defence, special_attack, gender, exp, wallet, inventory)
+        super().__init__(lineage, name, level, weapon, life, basic_attack, defence,
+                          {'nome': 'Sharpshooter', 'danno':special_attack}, gender, exp, wallet, inventory)
