4d734acf3877a0cf072c038e1e072518eb9b5710
Mr.Alsi <gabriel.alsina22@gmail.com>
2023-07-19 12:31:09 +0200

    aggiustato costruttori di knight, elf e ninja, fatto che è possibile prenderli dal DB

--- Classi/Ninja.py
+++ Classi/Ninja.py
@@ -1,10 +1,10 @@
 # creato da Gianluca 
 # Sottoclasse di Personaggio chiamata Ninja con attributi: 
 
-import Character;
+from Classi.Character import Character
 
 
 class Ninja(Character):
-    def __init__(self, gender, exp, wallet, inventory):
-        super().__init__(gender, exp, wallet, inventory)
+    def __init__(self, lineage, name, level, weapon, life, basic_attack, defence, special_attack, gender, exp, wallet, inventory):
+        super().__init__(lineage, name, level, weapon, life, basic_attack, defence, special_attack, gender, exp, wallet, inventory)
         # self.valid_weapons = ['shuriken']
--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 from Classi.Entity import Entity as newEntity
 from Classi.Character import Character as newCharacter
 from Classi.Wizard import Wizard as newWizard
+from Classi.Elf import Elf as newElf
+from Classi.Knight import Knigth as newKnigth
+from Classi.Ninja import Ninja as newNinja
 import DB.CRUD as db
 
 def loadCharacter(data):
@@ -9,16 +12,21 @@ def loadCharacter(data):
         #Create a wizard object
         return newWizard(data["lineage"], data["name"], data["level"], data["weapon"], data["life"], data["basic_attack"],
                 data["defence"], data["special_attack"], data["gender"], data["exp"], data["wallet"], data["inventory"])
-        
     elif data["type"] == "Elf":
-        pass
+        return newElf(data["lineage"], data["name"], data["level"], data["weapon"], data["life"], data["basic_attack"],
+                data["defence"], data["special_attack"], data["gender"], data["exp"], data["wallet"], data["inventory"])
+    elif data["type"] == "Ninja":
+        return newNinja(data["lineage"], data["name"], data["level"], data["weapon"], data["life"], data["basic_attack"],
+                data["defence"], data["special_attack"], data["gender"], data["exp"], data["wallet"], data["inventory"])
+    else:
+        return newKnigth(data["lineage"], data["name"], data["level"], data["weapon"], data["life"], data["basic_attack"],
+                data["defence"], data["special_attack"], data["gender"], data["exp"], data["wallet"], data["inventory"])
 
 
-
-dataPlayer = db.getCharacter("Nitro")
+dataPlayer = db.getCharacter("Strage")
 #print(dataPlayer)
 character = loadCharacter(dataPlayer)
-print(character.name)
+print(type(character))
 
 
 
