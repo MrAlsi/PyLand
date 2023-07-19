3fba07d11a60d7aea480e80c2ba67370b578c700
Federico Piras <fedepiras95@gmail.com>
2023-07-19 10:47:37 +0200

    da completare per gianlu

--- Classi/weapon_objects.py
+++ Classi/weapon_objects.py
@@ -14,9 +14,19 @@ Shuriken: shuriken ------------> Ninja.
 
 from Weapons import Weapon
 
+# Definire armi per Knight
 sword = Weapon(name="sward", damage=11, price=10, level=0)
+
+# Definire armi per Elf
 bow = Weapon(name="bow", damage=5, price=5, level=0)
+
+# Definire armi per Wizard
 magic_wand = Weapon(name="magic_wand", damage=12, price=7, level=0)
+
+# Definire armi per Ninja
 shuriken = Weapon(name="shuriken", damage=9, price=8, level=0)
 
 
+# Fai un dizionario di tipo: {"Tipo di PG (quindi Knight, Elf....)": [lista di armi per quel pg]
+# Esempio:  {"Knight": [sword, knife, alabarda],
+#             "Ninja": [armi del ninja]  }
