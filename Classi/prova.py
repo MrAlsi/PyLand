656672a16fcf9847df3cbf9ac376d72f85d60adf
Federico Piras <fedepiras95@gmail.com>
2023-07-19 15:08:14 +0200

    alcune funzioni aggiunte e testate

--- Classi/prova.py
+++ Classi/prova.py
@@ -1,5 +1,7 @@
 from Classi.Wizard import Wizard
+from Classi.Shops import Tavern, Smith
 from Classi.weapon_objects import weapons_dict
+
 
 wiz = Wizard(lineage="Buoni",
              name="Wiz",
@@ -11,5 +13,30 @@ wiz = Wizard(lineage="Buoni",
              special_attack=15,
              gender='m',
              exp=0,
-             wallet=30,
+             wallet=200,
              inventory=[])
+
+print(wiz.wallet)
+
+tavern = Tavern(name="Taverna da Boe")
+smith = Smith(name="Mario")
+
+# Compro arma
+list_of_available_weapons = smith.show_available_weapons(wiz)
+
+# Chiedo l'arma
+arma_name = input("Che arma vuoi? ")
+while arma_name not in list_of_available_weapons:
+    arma_name = input("Che arma vuoi? ")
+
+smith.buy_new_weapon(wiz, arma_name)
+
+# Compro arma
+smith.show_available_weapons(wiz)
+# Chiedo l'arma
+arma_name = input("Che arma vuoi? ")
+while arma_name not in list_of_available_weapons:
+    arma_name = input("Che arma vuoi? ")
+
+smith.buy_new_weapon(wiz, arma_name)
+
